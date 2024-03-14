842a6b82c9505ad12c5640b4512b5b870bcbdb6a
nandobfer <nandobfer@yahoo.com.br>
2024-03-13 21:19:13 -0300

    feat: getting movable

--- src/contexts/roomContext.tsx
+++ src/contexts/roomContext.tsx
@@ -1,15 +1,17 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import React from "react"
 import { Room } from "../types/server/class/Room"
 import { POSITION } from "../types/server/class/chess"
+import { ChessPiece } from "../types/server/class/ChessPiece"
+import { useIo } from "../hooks/useIo"
 
 interface RoomContextValue {
     room: Room | null
     setRoom: React.Dispatch<React.SetStateAction<Room | null>>
 
-    onSquareDrag: (from: POSITION, current_position: POSITION) => void
-
-    hoveredPosition: POSITION | null
+    onSquarePress: (piece: ChessPiece | null) => void
+    selectedPiece: ChessPiece | null
+    movablePositions: POSITION[]
 }
 
 interface RoomProviderProps {
@@ -21,22 +23,39 @@ const RoomContext = createContext<RoomContextValue>({} as RoomContextValue)
 export default RoomContext
 
 export const RoomProvider: React.FC<RoomProviderProps> = ({ children }) => {
+    const io = useIo()
+
     const [room, setRoom] = useState<Room | null>(null)
-    const [hoveredPosition, setHoveredPosition] = useState<POSITION | null>(null)
+    const [selectedPiece, setSelectedPiece] = useState<ChessPiece | null>(null)
+    const [movablePositions, setMovablePositions] = useState<POSITION[]>([])
 
-    const getSquare = (from: POSITION, x: number, y: number) => {
-        const squares_count_x = Math.floor(y / 40)
-        const squares_count_y = Math.floor(x / 40)
-
-        const target_position: POSITION = [from[0] + squares_count_x, from[1] + squares_count_y]
-        return target_position
+    const onSquarePress = (piece: ChessPiece | null) => {
+        setMovablePositions([])
+        if (piece) {
+            setSelectedPiece(piece)
+            io.emit("piece:movements", piece.position)
+        } else {
+            setSelectedPiece(null)
+        }
     }
 
-    const onSquareDrag = (from: POSITION, current_position: POSITION) => {
-        const target_position = getSquare(from, current_position[0], current_position[1])
-        console.log(target_position)
-        // setHoveredPosition(target_position)
-    }
+    useEffect(() => {
+        console.log({ movablePositions })
+    }, [movablePositions])
 
-    return <RoomContext.Provider value={{ room, setRoom, onSquareDrag, hoveredPosition }}>{children}</RoomContext.Provider>
+    useEffect(() => {
+        io.on("piece:movements", (positions: POSITION[]) => {
+            setMovablePositions(positions)
+        })
+
+        return () => {
+            io.off("piece:movements")
+        }
+    }, [])
+
+    return (
+        <RoomContext.Provider value={{ room, setRoom, onSquarePress: onSquarePress, selectedPiece, movablePositions }}>
+            {children}
+        </RoomContext.Provider>
+    )
 }
--- src/screens/Game/BoardSquareComponent.tsx
+++ src/screens/Game/BoardSquareComponent.tsx
@@ -4,6 +4,7 @@ import { Surface, Text } from "react-native-paper"
 import { POSITION } from "../../types/server/class/chess"
 import RoomContext from "../../contexts/roomContext"
 import schema from "../../style/colors.json"
+import { Pressable } from "react-native"
 
 interface BoardSquareComponentProps {
     piece: ChessPiece | null
@@ -11,27 +12,32 @@ interface BoardSquareComponentProps {
 }
 
 export const BoardSquareComponent: React.FC<BoardSquareComponentProps> = ({ piece, position }) => {
+    const { onSquarePress, selectedPiece, movablePositions } = useContext(RoomContext)
+
     const size = 40
     const even_row = position[0] % 2 === 0
     const even_column = position[1] % 2 === 0
     const white_square = schema.colors.onSurfaceVariant
     const black_square = schema.colors.onPrimary
     const square_color = even_row ? (even_column ? white_square : black_square) : even_column ? black_square : white_square
-
-    const { onSquareDrag } = useContext(RoomContext)
+    const is_movable = !!movablePositions.find((item) => item[0] == position[0] && item[1] == position[1])
 
     return (
-        <Surface
-            elevation={3}
-            style={{
-                width: size,
-                height: size,
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: square_color,
-            }}
-        >
-            <Text style={{ color: square_color == white_square ? black_square : white_square }}>{piece?.label}</Text>
-        </Surface>
+        <Pressable onPress={() => onSquarePress(piece)}>
+            <Surface
+                elevation={3}
+                style={{
+                    width: size,
+                    height: size,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: square_color,
+                    borderColor: selectedPiece && selectedPiece == piece ? "red" : is_movable ? "yellow" : "transparent",
+                    borderWidth: 2,
+                }}
+            >
+                <Text style={{ color: square_color == white_square ? black_square : white_square }}>{piece?.label}</Text>
+            </Surface>
+        </Pressable>
     )
 }
